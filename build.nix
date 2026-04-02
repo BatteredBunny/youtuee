2d0eae5b92238379a09bb6649978793bfb92609a
BatteredBunny <ayes2022@protonmail.com>
2026-04-03 00:03:06 +0300

    fix: Update nix hash

--- build.nix
+++ build.nix
@@ -8,7 +8,7 @@ buildGoModule {
   src = ./.;
 
   name = "youtuee";
-  vendorHash = "sha256-ul9o5SyvHpECslNba3OaoWOl5ZnTF/uQCqKwIbiSggc=";
+  vendorHash = "sha256-hPOTNgP/0ePDQ94Bb7aTIxTxgqUuv7zxw63ppEYiW28=";
 
   ldflags = [
     "-s"
